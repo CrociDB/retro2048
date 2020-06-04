d86ddbf86b780cd95d5648d7d83868d9abcec86c
Bruno Croci <crocidb@gmail.com>
2020-06-04 22:22:13 +0200

    finished draw box function

--- main.asm
+++ main.asm
@@ -19,33 +19,46 @@ clear:
     stosw
     loop clear
 
-    ; Drawing the line
-    mov ax, 0x3800
-    mov bh, 23                   ; Starts on column
-    mov dh, 37                   ; Prints X columns
-    call draw_square
+    ; Drawing the box
+    push 0x3800
+    push 0x0F25                 ; Rect size 37x15 (25x0F)
+    push 44                     ; Offset 23 chars on left
+    push 160 * 5                ; Offset 7 lines on top
+    call draw_box
 
 main_loop:
     jmp exit
 
     ;
-    ; Draw square function
-    ; Uses AX for color and char, BX for pos and DX for size
+    ; Draw box function
+    ; Params: (1) Line offset       (bp+2)
+    ;         (2) Row offset        (bp+4)
+    ;         (3) Box dimensions    (bp+6)
+    ;         (4) Char/Color        (bp+8)
     ;
-draw_square:
-    xor cx, cx                      ; Resets CX
-    mov cl, dh                      ; Copy length of line
+draw_box:
+    mov bp, sp                      ; Store the base of the stack, to get arguments
+    xor di, di                      ; Sets DI to screen origin
+    add di, [bp+2]                  ; Adds the line offset to DI
 
-    xor di, di                      ; Resets DI to the screen origin
-    push ax
-    xor ax, ax
-    mov al, bh                      ; Copy position of line start
-    shl al, 1                       ; Multiplies by two because chars are two bytes
-    add di, ax                      ; Adds the offset to DI
-    pop ax
+    mov dx, [bp+6]                  ; Copy dimensions of the box
+    mov ax, [bp+8]                  ; Copy the char/color to print
+    xor bx, bx
+    mov bl, dh                      ; Get the height of the box
+
+    xor cx, cx                      ; Resets CX
+    mov cl, dl                      ; Copy the width of the box
+    add di, [bp+4]                  ; Adds the line offset to DI
+
 draw_char:
     stosw
     loop draw_char
+
+    add word [bp+2], 160            ; Add a line (180 bytes) to offset
+    sub word [bp+6], 0x0100         ; Remove one line of height - it's 0x0100 because height is stored in the msb
+    mov cx, [bp+6]                  ; Copy the size of the box to test
+    cmp ch, 0                       ; Test the hight of the box
+    jnz draw_box                    ; If not zero, draw the rest of the box
     ret
 
 exit:
